8582d70dfd5c71043abd22fc1d2559bc8ea40efe
Abbas <abbasmuhammad4798@gmail.com>
2024-12-05 02:03:03 +0500

    add contact details and comment language button

--- components/layout/Header1.js
+++ components/layout/Header1.js
@@ -22,11 +22,11 @@ export default function Header({ topBarStyle, handleMobileMenuOpen }) {
                                 <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z">
                                     </path>
-                                </svg>Phone: +01-246-357 (Any time 24/7)</Link><Link className="email-icon" href="mailto:contact@transp.eu.com">
+                                </svg>Phone:+966 50 261 0367  (Any time 24/7)</Link><Link className="email-icon" href="mailto:contact@transp.eu.com">
                                     <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75">
                                         </path>
-                                    </svg>contact@transp.eu.com</Link></div>
+                                    </svg>moversriyadh1@gmail.com</Link></div>
                             <div className="col-lg-5 col-md-4 col-sm-7 col-8 text-end"><Link className="icon-socials icon-twitter2" href="#">
                                 <svg className="bi bi-twitter" xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="none" viewBox="0 0 16 16">
                                     <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z">
@@ -67,7 +67,7 @@ export default function Header({ topBarStyle, handleMobileMenuOpen }) {
                                     <span className="burger-icon-bottom" />
                                 </div>
                             </div>
-                            <div className="header-right">
+                            {/* <div className="header-right">
                                 <div className="d-inline-block box-dropdown-cart"><span className="font-lg icon-list icon-account"><span className="font-sm color-grey-900 arrow-down">
                                     <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418">
@@ -89,7 +89,7 @@ export default function Header({ topBarStyle, handleMobileMenuOpen }) {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z">
                                         </path>
                                     </svg>Get a quote</Link></div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -36,15 +36,15 @@ const PageHead = ({ headTitle, description, keywords, author }) => {
 
                 {/* Open Graph Meta Tags (for Facebook and LinkedIn sharing) */}
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content={headTitle || "Transp - Moving Services in Riyadh, Saudi Arabia"} />
-                <meta property="og:description" content={description || "Top moving services in Riyadh, Saudi Arabia. We offer reliable, fast, and professional moving and logistics solutions for individuals and businesses."} />
+                <meta property="og:title" content={headTitle || "Moving Services in Riyadh, Saudi Arabia"} />
+                <meta property="og:description" content={description || "Top moving services in Riyadh, Saudi Arabia. We offer reliable, fast, and professional moving solutions for individuals and businesses."} />
                 {/* <meta property="og:image" content="/path-to-your-image.jpg" />  Replace with the actual image URL */}
                 <meta property="og:url" content={typeof window !== "undefined" ? window.location.href : ""} />
                 
                 {/* Twitter Card Meta Tags */}
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={headTitle || "Transp - Moving Services in Riyadh, Saudi Arabia"} />
-                <meta name="twitter:description" content={description || "Top moving services in Riyadh, Saudi Arabia. We offer reliable, fast, and professional moving and logistics solutions for individuals and businesses."} />
+                <meta name="twitter:title" content={headTitle || "Moving Services in Riyadh, Saudi Arabia"} />
+                <meta name="twitter:description" content={description || "Top moving services in Riyadh, Saudi Arabia. We offer reliable, fast, and professional moving solutions for individuals and businesses."} />
                 <meta name="twitter:image" content="/path-to-your-image.jpg" /> {/* Replace with the actual image URL */}
                 <meta name="twitter:site" content="@yourtwitterhandle" /> {/* Replace with your Twitter handle */}
 
